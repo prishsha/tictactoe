82127db18f7eb884689703899319f4a75dea32fd
prishsha <prishavadhavkar@gmail.com>
2024-09-14 19:11:54 +0530

    Tie

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -40,39 +40,44 @@ const TicTacToe = () => {
             setCount(++count);
         }
         checkWin();
+        if(count===9)
+        {
+             titleRef.current.innerHTML=`It's a tie`;
+             reset();
+        }
     }
 
     const checkWin=()=> 
     {
-        if(data[0]==data[1] && data[1]==data[2] && data[2]!="")
+        if(data[0]===data[1] && data[1]===data[2] && data[2]!=="")
         {
             won(data[2]);
         }
-        else if(data[3]==data[4] && data[4]==data[5] && data[3]!="")
+        else if(data[3]===data[4] && data[4]===data[5] && data[3]!=="")
         {
             won(data[3]);
         }
-        else if(data[6]==data[7] && data[7]==data[8] && data[8]!="")
+        else if(data[6]===data[7] && data[7]===data[8] && data[8]!=="")
         {
             won(data[6]);
         }
-        else if(data[0]==data[3] && data[3]==data[6] && data[6]!="")
+        else if(data[0]===data[3] && data[3]===data[6] && data[6]!=="")
         {
             won(data[6]);
         }
-        else if(data[1]==data[4] && data[4]==data[7] && data[7]!="")
+        else if(data[1]===data[4] && data[4]===data[7] && data[7]!=="")
         {
             won(data[7]);
         }
-        else if(data[2]==data[5] && data[5]==data[8] && data[8]!="")
+        else if(data[2]===data[5] && data[5]===data[8] && data[8]!=="")
         {
             won(data[8]);
         }
-        else if(data[0]==data[4] && data[4]==data[8] && data[8]!="")
+        else if(data[0]===data[4] && data[4]===data[8] && data[8]!=="")
         {
             won(data[8]);
         }
-        else if(data[2]==data[4] && data[4]==data[6] && data[6]!="")
+        else if(data[2]===data[4] && data[4]===data[6] && data[6]!=="")
         {
             won(data[6]);
         }
@@ -81,7 +86,7 @@ const TicTacToe = () => {
     const won = (winner) =>
     {
         setLock(true);
-        if(winner=='x')
+        if(winner==='x')
         {
             titleRef.current.innerHTML=`Congrats <img src=${cross_icon}> wins`;
         }
